dda36354001ee73359352e45ab30fec62f6b5c3b
menne019 <menne019@morris.umn.edu>
2018-11-21 13:36:26 -0600

    Implement extended Euclidean algorithm

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 // Lab done by Khondoker Ahnaf Prio and Charles Menne
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -11,15 +13,60 @@ public class Main {
         BigInteger r0 = new BigInteger(sc.nextLine());
         System.out.println("Enter r1: ");
         BigInteger r1 = new BigInteger(sc.nextLine());
-        if (r0.compareTo(r1) != 0) {
-            System.out.println("Enter a new value for r1 (which has to be less than r0)");
-            r1 = new BigInteger(sc.nextLine());
 
+        printValues(extendedEuclidean(r0, r1));
+    }
 
+    /***
+     *
+     * @param r0
+     * @param r1
+     * @return Map<String, BigInteger> containing values returned from extended Euclidean algorithm
+     */
+    private static Map<String, BigInteger> extendedEuclidean(BigInteger r0, BigInteger r1) {
+        // Storing values returned from extended Euclidean algorithm
+        Map<String, BigInteger> euclidValues = new HashMap<>();
+        euclidValues.put("r0", r0);
+        euclidValues.put("r1", r1);
 
+        // Initializing q, s, and t for algorithm, Last and Holders for intermediary steps
+        BigInteger q, s, tLast;
+        q = s = tLast = BigInteger.valueOf(0);
+
+        BigInteger t, sLast;
+        t = sLast = BigInteger.valueOf(1);
+
+        BigInteger r0Holder, tHolder, sHolder;
+
+        // Start of extended Euclidean algorithm
+        // Could have been done recursively, but we decided not to
+        while (!r1.equals(BigInteger.valueOf(0))) {
+
+            q = r0.divide(r1);
+
+            r0Holder = r0;
+            r0 = r1;
+            r1 = r0Holder.mod(r1);
+
+            sHolder = s;
+            s = (sLast.subtract(q.multiply(sHolder)));
+            sLast = sHolder;
+
+            tHolder = t;
+            t = (tLast.subtract(q.multiply(tHolder)));
+            tLast = tHolder;
         }
 
+        euclidValues.put("s", sLast);
+        euclidValues.put("t", tLast);
+        euclidValues.put("gcd", r0);
 
+        return euclidValues;
+    }
 
+    private static void printValues(Map<String, BigInteger> values) {
+        for(Map.Entry<String, BigInteger> entry : values.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
     }
 }
